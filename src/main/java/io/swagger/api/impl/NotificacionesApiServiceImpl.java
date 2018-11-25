fd2325676062c3d7fba57ad9539c5ff9d0d7f1e0
Khamaleona <ircarrasc@alumnos.unex.es>
2018-11-25 23:09:53 +0000

    Correcciones aplicadas. PUT y GET funcionan perfectamente. POST y DELETE no (error Jersey).

--- src/main/java/io/swagger/api/impl/NotificacionesApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/NotificacionesApiServiceImpl.java
@@ -6,7 +6,7 @@ import io.swagger.api.Entities.Notificacion;
 
 import java.util.ArrayList;
 import java.util.Date;
-
+import java.util.Calendar;
 
 import io.swagger.api.NotFoundException;
 
@@ -21,15 +21,40 @@ public class NotificacionesApiServiceImpl extends NotificacionesApiService {
 
     private ArrayList<Notificacion> notificaciones = new ArrayList<>();
     
+    private void inicializarNotificaciones(){
+        Notificacion n1 = new Notificacion("Irene","Alerta1","Notificacion1");
+        Notificacion n2 = new Notificacion("Victor", "Alerta2", "Notificacion2");
+        Notificacion n3 = new Notificacion("Irene", "Alerta3", "Notificacion3");
+        
+        Calendar cal = Calendar.getInstance();
+        
+        n1.setFecha(cal.getTime());
+        n1.setContacto("617929906");
+        n1.setEstado("Pendiente");
+        
+        n2.setFecha(cal.getTime());
+        n2.setContacto("617929906");
+        n2.setEstado("Pendiente");
+        
+        n3.setFecha(cal.getTime());
+        n3.setContacto("617929906");
+        n3.setEstado("Pendiente");
+        
+        notificaciones.add(n1);
+        notificaciones.add(n2);
+        notificaciones.add(n3);
+    }
+    
     @Override
     public Response actualizarNotificacion( @NotNull String usuarioId,  @NotNull String notificacionId,
                                             String alertaId,  Date fecha,  String estado,  String destinatario, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
         Boolean flag = false;
         Notificacion notificacion;
+        inicializarNotificaciones();
 
         for (int i=0; i<notificaciones.size(); i++){
-            if(notificaciones.get(i).getNotificacionId() == notificacionId){
+            if(notificaciones.get(i).getNotificacionId().equals(notificacionId)){
                 flag = true;
 
                 notificacion = notificaciones.get(i);
@@ -57,9 +82,11 @@ public class NotificacionesApiServiceImpl extends NotificacionesApiService {
         // do some magic!
 
         Boolean flag = false;
+        inicializarNotificaciones();
 
         for(int i=0; i<notificaciones.size(); i++){
-            if(notificaciones.get(i).getNotificacionId() == notificacionId){
+            if(notificaciones.get(i).getNotificacionId().equals(notificacionId) && 
+                notificaciones.get(i).getUsuarioId().equals(usuarioId)){
                 flag = true;
                 notificaciones.remove(i);
             }
@@ -75,13 +102,16 @@ public class NotificacionesApiServiceImpl extends NotificacionesApiService {
     @Override
     public Response crearNotificacion( @NotNull String usuarioId,  @NotNull String alertaId, SecurityContext securityContext) throws NotFoundException {
 
-        String notificacionId = String.valueOf(notificaciones.size()-1);
+        inicializarNotificaciones();
+        String notificacionId = String.valueOf(notificaciones.size());
 
         Notificacion notificacion = new Notificacion(usuarioId, alertaId, notificacionId);
         if(notificaciones.add(notificacion)){
             return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
         }else{
-            return Response.serverError().entity(new ApiResponseMessage(ApiResponseMessage.ERROR, "No ha sido posible insertar.")).build();
+            String cadena = "No ha sido posible insertar. ";
+            cadena+= usuarioId + " - " + alertaId;
+            return Response.serverError().entity(new ApiResponseMessage(ApiResponseMessage.ERROR, cadena)).build();
         }
     }
     
@@ -89,8 +119,10 @@ public class NotificacionesApiServiceImpl extends NotificacionesApiService {
     public Response obtenerNotificaciones( @NotNull String usuarioId, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
         ArrayList<Notificacion> results = new ArrayList<>();
+        inicializarNotificaciones();
+        
         for (Notificacion n: notificaciones) {
-            if(n.getUsuarioId() == usuarioId){
+            if(n.getUsuarioId().equals(usuarioId)){
                 results.add(n);
             }
         }
@@ -101,7 +133,7 @@ public class NotificacionesApiServiceImpl extends NotificacionesApiService {
 
             return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, JSON)).build();
         }else{
-            return Response.serverError().entity(new ApiResponseMessage(ApiResponseMessage.ERROR, "No ha sido posible insertar.")).build();
+            return Response.serverError().entity(new ApiResponseMessage(ApiResponseMessage.ERROR, "No ha sido posible obtener las notificaciones.")).build();
         }
     }
     
